d5062d0b8b64629f5e5712d4e50ca9617af2c30c
Jim Shaw <jimxshaw@gmail.com>
2024-10-03 16:13:58 -0500

    Update code to remove warnings.

--- GettingStarted/Ch03/sizeof.c
+++ GettingStarted/Ch03/sizeof.c
@@ -2,21 +2,21 @@
 
 int main(void)
 {
-  printf("\nchar                  is %d bytes", sizeof(char));
-  printf("\nint                   is %d bytes", sizeof(int));
-  printf("\nshort                 is %d bytes", sizeof(short));
-  printf("\nlong                  is %d bytes", sizeof(long));
-  printf("\nlong long             is %d bytes\n", sizeof(long long));
+  printf("\nchar                  is %lu bytes", sizeof(char));
+  printf("\nint                   is %lu bytes", sizeof(int));
+  printf("\nshort                 is %lu bytes", sizeof(short));
+  printf("\nlong                  is %lu bytes", sizeof(long));
+  printf("\nlong long             is %lu bytes\n", sizeof(long long));
 
-  printf("\nunsigned char         is %d bytes", sizeof(unsigned char));
-  printf("\nunsigned int          is %d bytes", sizeof(unsigned int));
-  printf("\nunsigned short        is %d bytes", sizeof(unsigned short));
-  printf("\nunsigned long         is %d bytes", sizeof(unsigned long));
-  printf("\nunsigned long long    is %d bytes\n", sizeof(unsigned long long));
+  printf("\nunsigned char         is %lu bytes", sizeof(unsigned char));
+  printf("\nunsigned int          is %lu bytes", sizeof(unsigned int));
+  printf("\nunsigned short        is %lu bytes", sizeof(unsigned short));
+  printf("\nunsigned long         is %lu bytes", sizeof(unsigned long));
+  printf("\nunsigned long long    is %lu bytes\n", sizeof(unsigned long long));
 
-  printf("\nfloat                 is %d bytes", sizeof(float));
-  printf("\ndouble                is %d bytes", sizeof(double));
-  printf("\nlong double           is %d bytes\n", sizeof(long double));
+  printf("\nfloat                 is %lu bytes", sizeof(float));
+  printf("\ndouble                is %lu bytes", sizeof(double));
+  printf("\nlong double           is %lu bytes\n", sizeof(long double));
 
   return 0;
 }
