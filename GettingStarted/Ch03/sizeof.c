cdb58f6add83a5ef1b2010aa704d7464b4c73b13
Jim Shaw <jimxshaw@gmail.com>
2024-10-03 16:34:52 -0500

    Fix warnings.

--- GettingStarted/Ch03/sizeof.c
+++ GettingStarted/Ch03/sizeof.c
@@ -2,21 +2,21 @@
 
 int main(void)
 {
-  printf("\nchar                  is %lu bytes", sizeof(char));
-  printf("\nint                   is %lu bytes", sizeof(int));
-  printf("\nshort                 is %lu bytes", sizeof(short));
-  printf("\nlong                  is %lu bytes", sizeof(long));
-  printf("\nlong long             is %lu bytes\n", sizeof(long long));
+  printf("\nchar                  is %zu bytes", sizeof(char));
+  printf("\nint                   is %zu bytes", sizeof(int));
+  printf("\nshort                 is %zu bytes", sizeof(short));
+  printf("\nlong                  is %zu bytes", sizeof(long));
+  printf("\nlong long             is %zu bytes\n", sizeof(long long));
 
-  printf("\nunsigned char         is %lu bytes", sizeof(unsigned char));
-  printf("\nunsigned int          is %lu bytes", sizeof(unsigned int));
-  printf("\nunsigned short        is %lu bytes", sizeof(unsigned short));
-  printf("\nunsigned long         is %lu bytes", sizeof(unsigned long));
-  printf("\nunsigned long long    is %lu bytes\n", sizeof(unsigned long long));
+  printf("\nunsigned char         is %zu bytes", sizeof(unsigned char));
+  printf("\nunsigned int          is %zu bytes", sizeof(unsigned int));
+  printf("\nunsigned short        is %zu bytes", sizeof(unsigned short));
+  printf("\nunsigned long         is %zu bytes", sizeof(unsigned long));
+  printf("\nunsigned long long    is %zu bytes\n", sizeof(unsigned long long));
 
-  printf("\nfloat                 is %lu bytes", sizeof(float));
-  printf("\ndouble                is %lu bytes", sizeof(double));
-  printf("\nlong double           is %lu bytes\n", sizeof(long double));
+  printf("\nfloat                 is %zu bytes", sizeof(float));
+  printf("\ndouble                is %zu bytes", sizeof(double));
+  printf("\nlong double           is %zu bytes\n", sizeof(long double));
 
   return 0;
 }
