88f8ec3f8cc072429f5c34b9d5fddde344b3e9ea
Jim Shaw <jimxshaw@gmail.com>
2024-10-11 21:16:49 -0500

    Update exercise 3.

--- GettingStarted/Ch05/exercise3.c
+++ GettingStarted/Ch05/exercise3.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
-int a, b, product;
+int a, b, result;
 
 int multiply(int a, int b);
+int divide(int x, int y);
 
 int main(void)
 {
   puts("Input two integers: ");
   scanf("%d %d", &a, &b);
 
-  product = multiply(a, b);
+  result = multiply(a, b);
 
-  printf("product: %d\n", product);
+  printf("product: %d\n", result);
+
+  result = divide(a, b);
+
+  if (result == 9999)
+  {
+    puts("quotient: cannot divide by 0!");
+  }
+  else
+  {
+    printf("quotient: %d\n", result);
+  }
 
   return 0;
 }
@@ -19,4 +31,14 @@ int main(void)
 int multiply(int a, int b)
 {
   return a * b;
+}
+
+int divide(int x, int y)
+{
+  if (y == 0)
+  {
+    return 9999;
+  }
+
+  return x / y;
 }
