c88e49ed9e1f1bfef2e75b9f045aeda44be950aa
Jim Shaw <jimxshaw@gmail.com>
2024-10-21 17:30:15 -0500

    Add Ch 8 2 dimensional array example.

---
+++ GettingStarted/Ch08/scoring.c
@@ -0,0 +1,16 @@
+/* Shows how to use 2 dimensional arrays */
+#include <stdio.h>
+
+#define PLAYERS 5
+#define GAMES 4
+
+// The [0] elements will be ignored for simplicity.
+int scores[6][5];
+float score_average, best_average;
+int point_total, best_player;
+int counter1, counter2;
+
+int main(void)
+{
+  return 0;
+}
