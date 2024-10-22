9a570f7fa94061eb9b99e5b58416ac66e47c3df0
Jim Shaw <jimxshaw@gmail.com>
2024-10-22 12:39:35 -0500

    Update Ch 8 exercises.

--- GettingStarted/Ch08/exercise.c
+++ GettingStarted/Ch08/exercise.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define EIGHTYEIGHT 88
-#define FIRST 12
-#define SECOND 10
-#define INITIAL 0
+#define FIRST 5
+#define SECOND 4
 
 int eighty_eight[EIGHTYEIGHT], stuff[FIRST][SECOND];
 int outer, inner;
 
 int main(void)
 {
+  srand(time(NULL));
+
   for (int i = 0; i < EIGHTYEIGHT; i++)
   {
     eighty_eight[i] = EIGHTYEIGHT;
@@ -21,7 +23,7 @@ int main(void)
   {
     for (inner = 0; inner < SECOND; inner++)
     {
-      stuff[outer][inner] = INITIAL;
+      stuff[outer][inner] = rand();
       printf("stuff[%d][%d] = %d\n", outer, inner, stuff[outer][inner]);
     }
   }
