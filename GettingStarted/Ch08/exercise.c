cba686bec05bdff01d067dffdafe9c52d58406f9
Jim Shaw <jimxshaw@gmail.com>
2024-10-22 12:30:10 -0500

    Update Ch 8 exercises.

--- GettingStarted/Ch08/exercise.c
+++ GettingStarted/Ch08/exercise.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 #define EIGHTYEIGHT 88
+#define FIRST 12
+#define SECOND 10
+#define INITIAL 0
 
-int eighty_eight[EIGHTYEIGHT];
+int eighty_eight[EIGHTYEIGHT], stuff[FIRST][SECOND];
+int outer, inner;
 
 int main(void)
 {
@@ -11,6 +15,15 @@ int main(void)
   {
     eighty_eight[i] = EIGHTYEIGHT;
     printf("eighty_eight[%d] = %d\n", i, eighty_eight[i]);
+  }
+
+  for (outer = 0; outer < FIRST; outer++)
+  {
+    for (inner = 0; inner < SECOND; inner++)
+    {
+      stuff[outer][inner] = INITIAL;
+      printf("stuff[%d][%d] = %d\n", outer, inner, stuff[outer][inner]);
+    }
   }
 
   return 0;
