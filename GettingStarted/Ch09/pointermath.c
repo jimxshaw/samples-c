e2b1553011ae9a24ac247ed0f38b09c3ab6a2b5f
jimxshaw <jimxshaw@gmail.com>
2024-10-29 11:05:14 -0500

    Update pointermath example.

--- GettingStarted/Ch09/pointermath.c
+++ GettingStarted/Ch09/pointermath.c
@@ -2,7 +2,7 @@
   array elements with pointer notation. */
 #include <stdio.h>
 
-#define MAX 10
+#define MAX 5
 
 int count;
 
@@ -12,11 +12,16 @@ float f_array[MAX] = {.0, .1, .2, .3, .4};
 int *i_pointer;
 float *f_pointer;
 
-// Initialize the pointers.
-i_pointer = i_array;
-f_pointer = f_array;
-
 main(void)
 {
+  // Initialize the pointers.
+  i_pointer = i_array;
+  f_pointer = f_array;
+
+  for (count = 0; count < MAX; count++)
+  {
+    printf("%d\t%f\n", *i_pointer++, *f_pointer++);
+  }
+
   return 0;
 }
