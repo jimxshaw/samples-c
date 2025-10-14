8ff4dd4c00ee6c2fbbd7243c744b391bf2145876
Jim Shaw <jimxshaw@gmail.com>
2025-10-14 10:21:02 -0500

    Add boilterplate scaffolding.

--- FindTheBison/main.cpp
+++ FindTheBison/main.cpp
@@ -1,4 +1,22 @@
+/*
+Program Name: Find the Bison
+Date: 10/12/2025
+Author: James Shaw
+Purpose: Count the number of open "((" and closed "))" pairs of parentheses,
+          which represents Bison legs.
+*/
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <locale>
+#include <iomanip>
+#include <stdexcept>
+
+#include "LineInfo.h"
+
+using namespace std;
+using namespace chrono;
 
 int main()
 {
