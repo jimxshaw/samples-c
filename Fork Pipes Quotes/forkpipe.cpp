d9c8cc3d3f5413762df694c8e47189d4e26d50bd
jimxshaw <jimxshaw@gmail.com>
2025-10-22 14:16:38 -0500

    Add Fork Pipes Quotes sample.

---
+++ Fork Pipes Quotes/forkpipe.cpp
@@ -0,0 +1,28 @@
+/*
+Program Name: Fork Pipes Quotes
+Date: 10/22/2025
+Author: James Shaw
+Purpose: Loads quotes from a file. Create two pipes, one for
+        messages from parent to child, one for messages
+        from child to parent. Forks a new process. The parent
+        sends requests like "Get Quote" or "Exit" to the child
+        through the pipe. The child reads those requests,
+        responds with quotes and sends them back. After a
+        certain number of quotes, the parent tells the child
+        to exit. Parent and child terminate gracefully.
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdexcept>
+
+#include <LineInfo.h>
+
+using namespace std;
+
+
