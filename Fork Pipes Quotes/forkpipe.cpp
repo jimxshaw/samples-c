79f0c796bcc5c1c14297fd053ea3d49aa29e5e48
Jim Shaw <jimxshaw@gmail.com>
2025-10-24 20:34:56 -0500

    Add if statements when creating pipes.

--- Fork Pipes Quotes/forkpipe.cpp
+++ Fork Pipes Quotes/forkpipe.cpp
@@ -25,9 +25,18 @@ Purpose: Loads quotes from a file. Create two pipes, one for
 
 using namespace std;
 
-const int MAX_BUFFER_SIZE = 1000;
+static const char *const QUOTES_FILE_NAME = "quotes.txt";
 
+const int MAX_BUFFER_SIZE = 1000;
 const int MAX_QUOTES = 1000;
+
+const int READ = 0;
+const int WRITE = 1;
+
+const int PIPE_ERROR = -1;
+const int FORK_ERROR = -1;
+
+const int CHILD_PID = 0;
 
 const char GET_QUOTE_MESSAGE[] = "Get Quote";
 const char EXIT_MESSAGE[] = "Exit";
@@ -63,8 +72,17 @@ int main(int argc, char *argv[])
       throw domain_error(LineInfo("Usage: ./forkpipe <number of quotes to request>", __FILE__, __LINE__));
     }
 
-    // Convert argument to integer.
+    // Convert argument to integer, atoi is ascii to integer.
+    // argv[0] is the program name and argv[1] is the number.
     int numQuotesToRequest = atoi(argv[1]);
+    char *lines[1000];
+    unsigned numOfLines;
+
+    // Retrieve the quotes array from the file.
+    getQuotesArray(lines, numOfLines);
+
+    // Process ID.
+    int pid;
 
     // Declare file descriptor pipe arrays.
     // Child will read from [0], parent will write to [1].
@@ -74,6 +92,18 @@ int main(int argc, char *argv[])
     int pipeParentReadWhileWrite[2];
 
     // TODO: Create the pipes. Throw if fail.
+    if (pipe(pipeParentWriteChildRead) == PIPE_ERROR)
+    {
+      throw domain_error(LineInfo("Error: could not create pipe for pipeParentWriteChildRead", __FILE__, __LINE__));
+    }
+
+    if (pipe(pipeParentReadWhileWrite) == PIPE_ERROR)
+    {
+      throw domain_error(LineInfo("Error: could not create pipe for pipeParentReadWhileWrite", __FILE__, __LINE__));
+    }
+
+    cout << endl
+         << endl;
 
     // TODO: Load quotes from the file into memory.
 
